0c7f9df13e3fef5cd352940ab565a6680ed8ab9a
EUGENE OSEI SABIE <sabieeugeneosei@yahoo.com>
2025-05-03 17:23:19 +0000

    Booking

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="#contact"
+                  href="https://calendly.com/sabieeugeneosei177/30min"
                   className="btn btn-custom btn-lg page-scroll"
                 >
                   Book an appointment
